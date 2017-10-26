46b6c93f1ca0a5728b79a9be1ad4da816a0555d7
hwwq <554344680@qq.com>
2017-10-26 22:18:20 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/xzbm/andriod/db/Province.java
@@ -0,0 +1,33 @@
+package com.xzbm.andriod.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by sss55 on 2017/10/26.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
